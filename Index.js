fb8bf16790389fbbc4942fb3186e001a01b223f9
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-03-07 21:15:42 -0500

    basecode

---
+++ Index.js
@@ -0,0 +1,10 @@
+// #Start your Engines
+
+function fn(...c) {
+    if (!c.every((a) => Number.isNumber(a)))
+      throw "All arguments must be numbers.";
+    return c.reduce((a, b) => a + b);
+  }
+  
+  fn(1, 2, 3); //> 6
+  fn(10, "B", 20); //> error All arguments must be numbers.
